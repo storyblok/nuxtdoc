ee27d9116075fc6c018676e006d7cbc332906f3f
Dominik Angerer <dominikangerer1@gmail.com>
2018-04-09 10:51:07 +0200

    Generate routes according to the links API.

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,15 +47,47 @@ module.exports = {
     ['storyblok-nuxt', {accessToken: 'R5hUgUB9PGoRq2XwtYw14wtt', cacheProvider: 'memory'}]
   ],
   generate: {
-    routes: function () {
-      return axios.get('https://api.storyblok.com/v1/cdn/links?token=R5hUgUB9PGoRq2XwtYw14wtt&version=draft').then((res) => {
-        let routes = []
+    routes: function (callback) {
+      const token = `R5hUgUB9PGoRq2XwtYw14wtt`
+      const per_page = 100
+      const version = `draft`
+      
+      let page = 1
+      let routes = []
+
+      // Call first Page of the Links API: https://www.storyblok.com/docs/Delivery-Api/Links
+      axios.get(`https://api.storyblok.com/v1/cdn/links?token=${token}&version=${version}&per_page=${per_page}&page=${page}&cv=${Date.now()}`).then((res) => {
         Object.keys(res.data.links).forEach((key) => {
           if (res.data.links[key].slug != 'home') {
             routes.push('/' + res.data.links[key].slug)
           }
         })
-        return routes
+
+        // Check if there are more pages available otherwise execute callback with current routes.
+        const total = res.headers.total
+        const maxPage = Math.ceil(total / per_page)
+        if(maxPage <= 1) {
+          callback(null, routes)
+        }
+
+        // Since we know the total we now can pregenerate all requests we need to get all Links
+        let contentRequests = [] 
+        for (let page = 2; page <= maxPage; page++) {
+          contentRequests.push(axios.get(`https://api.storyblok.com/v1/cdn/links?token=${token}&version=${version}&per_page=${per_page}&page=${page}&cv=${Date.now()}`))
+        }
+
+        // Axios allows us to exectue all requests using axios.spread we will than generate our routes and execute the callback
+        axios.all(contentRequests).then(axios.spread((...requests) => {
+          requests.forEach((request) => {
+            Object.keys(request.data.links).forEach((key) => {
+              if (request.data.links[key].slug != 'home') {
+                routes.push('/' + request.data.links[key].slug)
+              }
+            })
+          })
+        
+          callback(null, routes)
+        })).catch(callback)
       })
     }
   },
