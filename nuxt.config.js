414fff7b962e33c733eb78d48ebbeeb4d8886841
Dominik Angerer <dominikangerer1@gmail.com>
2018-04-09 10:52:38 +0200

    deploy netlify

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'A setup to build beautiful documentation with Nuxt and Storyblok' }
+      { hid: 'description', name: 'description', content: 'A setup to build beautiful documentation with Nuxt and Storyblok deployed on Netlify' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
