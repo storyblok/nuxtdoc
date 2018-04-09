7bc8bd30f68ca56620602fbfcef853e367d43dfa
Dominik Angerer <dominikangerer1@gmail.com>
2018-04-10 01:32:30 +0200

    adds better title

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'nuxtdoc',
+    title: 'NuxtDoc - Combining Nuxt.js, Storyblok and Netlify',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
